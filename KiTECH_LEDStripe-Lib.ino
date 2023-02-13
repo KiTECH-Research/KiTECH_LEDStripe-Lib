ad09734a8b66ec0197084556af26d80eddbf8a22
Katka Marcincakova <katulik137@gmail.com>
2023-02-13 23:41:13 +0100

    vyhodeny oled a pridane tlacidlo bez kniznice

--- KiTECH_LEDStripe-Lib.ino
+++ KiTECH_LEDStripe-Lib.ino
@@ -1,30 +1,34 @@
 #include "KiTECH_LEDStripe.h"
-#include "KiTECH_OLED.h"
 
 KiTECH_LEDStripe stripe;
-KiTECH_OLED oled;
+
+bool stripeOn = false;
+unsigned long prev_millis = 0;
 
 void setup() {
     Serial.begin(9600);
     stripe.setup();
 
-    if (oled.setup()) {
-        oled.print("setup OK");
-    } else {
-        oled.print("setup FAIL");
-    }
-    oled.clear();    
+    pinMode(4, INPUT);
 }
 
 void loop() {
-    int r = stripe.get_color_value(stripe.Red);
-    int g = stripe.get_color_value(stripe.Green);
-    int b = stripe.get_color_value(stripe.Blue);
-    oled.print_rgb_values(r, g, b);
-    stripe.show_color(r, g, b);
-    Serial.print(r);
-    Serial.print(" ");
-    Serial.print(g);
-    Serial.print(" ");
-    Serial.println(b);
+    if (stripeOn) {
+        int r = stripe.get_color_value(stripe.Red);
+        int g = stripe.get_color_value(stripe.Green);
+        int b = stripe.get_color_value(stripe.Blue);
+        stripe.show_color(r, g, b);
+        Serial.print(r);
+        Serial.print(" ");
+        Serial.print(g);
+        Serial.print(" ");
+        Serial.println(b);
+    } else {
+        stripe.show_color(0, 0, 0);
+    }
+
+    if (digitalRead(4) == HIGH && (millis() - prev_millis > 250)) {
+        prev_millis = millis();
+        stripeOn = !stripeOn;
+    }
 }
